e8925b1d6c6a7a77122835e42ce7727a63e25f6b
CodexPremiera <ashleyken.professional@gmail.com>
2024-04-10 14:09:21 +0800

    add vertical and diagonal check

--- app/src/main/java/task/match_three_game/logic/Grid.java
+++ app/src/main/java/task/match_three_game/logic/Grid.java
@@ -11,8 +11,12 @@ import task.match_three_game.R;
 
 public class Grid {
     /* FIELDS */
-    private ArrayList<Column> columns;
+    private final ArrayList<Column> columns;
     private int numberOfColumns;
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
 
     /* CONSTRUCTOR */
     public Grid() {
@@ -30,29 +34,63 @@ public class Grid {
         if (originY >= column.getSize())
             throw new RuntimeException("Column is full.");
 
-        Cell origin = columns.get(originX).get(originY);
-
-        // init range
-        int minX, maxX, minY, maxY;
+        // initialize range
         minX = maxX = originX;
         minY = maxY = originY;
+        setRange(originX, originY);
 
-        // set range
+        // check horizontal, vertical, and diagonal
+        if (checkHorizontal(originX, originY))
+            return true;
+        if (checkVertical(originX, originY))
+            return true;
+        return checkDiagonal(originX, originY);
+    }
+
+    private void setRange(int originX, int originY) {
         while (minX > 0 && minX > originX - 2) minX--;
         while (minY > 0 && minY > originY - 2) minY--;
         while (maxX < this.numberOfColumns && maxX < originX + 3) maxX++;
         while (maxY < this.numberOfColumns && maxY < originY + 3) maxY++;
+    }
 
-        // check horizontal
+    private boolean checkHorizontal(int originX, int originY) {
+        Cell origin = columns.get(originX).get(originY);
         int connectCount = 0;
+
         for (int col = minX; col < maxX; col++) {
             Cell currentCell = columns.get(col).get(originY);
             connectCount = (currentCell.getColorist() == origin.getColorist()) ? connectCount + 1 : 0;
 
-            if (connectCount == 3) {
-                columns.get(0).get(numberOfColumns - 1).setColor(Color.BLACK);
-                return true;
-            }
+            if (connectCount == 3) return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkVertical(int originX, int originY) {
+        Cell origin = columns.get(originX).get(originY);
+        int connectCount = 0;
+
+        for (int row = minY; row < maxY; row++) {
+            Cell currentCell = columns.get(originX).get(row);
+            connectCount = (currentCell.getColorist() == origin.getColorist()) ? connectCount + 1 : 0;
+
+            if (connectCount == 3) return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkDiagonal(int originX, int originY) {
+        Cell origin = columns.get(originX).get(originY);
+        int connectCount = 0;
+
+        for (int col = minX, row = minY; col < maxX && row < maxY; col++, row++) {
+            Cell currentCell = columns.get(col).get(row);
+            connectCount = (currentCell.getColorist() == origin.getColorist()) ? connectCount + 1 : 0;
+
+            if (connectCount == 3) return true;
         }
 
         return false;
